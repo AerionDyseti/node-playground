cbba8c76daadb168616c849f9b0f0e8f3620a3cd
Kevin Whiteside <whiteside.kj@gmail.com>
2022-01-10 21:09:05 -0600

    Stuff

---
+++ die.js
@@ -0,0 +1,9 @@
+class Die {
+    constructor(param1) {
+        this.prop1 = param1;
+    }
+}
+
+var thisDie = new Die("boop");
+
+console.log(thisDie.prop1);
--- index.js
+++ index.js
@@ -1,19 +1,50 @@
-let doNothing = require("./otherCode.js");
-
 sayHello()
 sayHello("Shea")
-doNothing();
+
+let thingy = {
+    propA: "A",
+    propB: 12,
+    propC: null,
+    funcProp: (a,b) => { return a + b }
+}
+
+let boo = thingy.funcProp(1,2);
+console.log(boo)
+
+let thingies = [1, 2, 3, "boop", { foo: ""}, undefined, null];
+
+thingies.forEach(x => {
+    if (x === "boop")
+    {
+        console.log("YAY")
+    }
+})
+var numbers = thingies.filter(x => x == 1 || x == 2)
+console.log(numbers);
+
+let func = (param1, param2) => {
+    console.log(param1)
+    console.log(param2)
+}
+doAThing(func, thingy.propA, thingy.propB)
 
 // Functions
 
 function sayHello(nameToWrite)
 {
-    let name = "World";
+    let name = "World"
 
     if (nameToWrite !== undefined)
     {
         name = nameToWrite
     }
 
-    console.log(`Hello, ${name}!`);
+    console.log(`Hello, ${name}!`)
+}
+
+function doAThing(thingToDo, ...params)
+{
+    console.log("DOINGATHING")
+    thingToDo(...params)
+    console.log("DID THE THING");
 }
